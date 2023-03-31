740cede056b8f34fcb362446575076f189bca6d9
nicoandrade50 <83848730+nicoandrade50@users.noreply.github.com>
2023-03-31 11:25:42 -0500

    Add files via upload

---
+++ src/api/animall/controllers/animall.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * animall controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::animall.animall');
---
+++ src/api/animall/services/animall.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * animall service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::animall.animall');
